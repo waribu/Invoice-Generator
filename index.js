c203f3a38b1ee9f75096d7806c619ca8f7b034ab
Waribu George <gwaribu@gmail.com>
2022-06-28 23:09:59 +0300

    v.1

--- index.js
+++ index.js
@@ -1,60 +1,87 @@
-//Import the library into your project
-var easyinvoice = require("easyinvoice");
-var fs = require("fs");
-
+const easyinvoice = require("easyinvoice");
+const fs = require("fs");
+// Our new data object, this will replace the empty object we used earlier.
 var data = {
-  //"documentTitle": "RECEIPT", //Defaults to INVOICE
-  currency: "usd",
-  taxNotation: "vat", //or gst
-  marginTop: 25,
-  marginRight: 25,
-  marginLeft: 25,
-  marginBottom: 25,
-  logo: "https://freepikpsd.com/logo-template-png-transparent-images-free/162789/", //or base64
-  //"logoExtension": "png", //only when logo is base64
-  sender: {
-    company: "Ributech Computers",
-    phone: "0722691006",
-    email: "info@ributechcomputers.com",
-    city: "Nairobi",
-    country: "Kenya",
-    //"custom1": "custom value 1",
-    //"custom2": "custom value 2",
-    //"custom3": "custom value 3"
-  },
+  // recipient's details
   client: {
     company: "Mr. Bean",
-    phone: "0720000000",
-    email: "bean@gmail.com",
+    address: "Blue Hut Building",
+    zip: "4000 CD",
     city: "Nairobi",
     country: "Kenya",
-    //"custom1": "custom value 1",
-    //"custom2": "custom value 2",
-    //"custom3": "custom value 3"
   },
-  invoiceNumber: "2020.0001",
-  invoiceDate: "05-01-2022",
+
+  // Now let's add our own sender details
+  sender: {
+    company: "Ributech Computers",
+    address: "Bihi Towers",
+    zip: "1234 AB",
+    city: "Nairobi",
+    country: "Kanya",
+  },
+
+  // Of course we would like to use our own logo and/or background on this invoice. There are a few ways to do this.
+  images: {
+    //      Logo:
+    // 1.   Use a url
+    logo: "https://public.easyinvoice.cloud/img/logo_en_original.png",
+  },
+
+  // Let's add some standard invoice data, like invoice number, date and due-date
+  information: {
+    // Invoice number
+    number: "2022.0561",
+    // Invoice data
+    date: "12-12-2022",
+    // Invoice due date
+    "due-date": "31-12-2022",
+  },
+
+  // Now let's add some products! Calculations will be done automatically for you.
   products: [
     {
-      quantity: "4",
-      description: "Laptop Keyboard",
-      tax: 16,
-      price: 2500,
+      quantity: "2",
+      description: "laptop screen",
+      "tax-rate": 16,
+      price: 55.0,
     },
     {
-      quantity: "2",
-      description: "Screen Replacement",
-      tax: 16,
-      price: 5000,
+      quantity: "4",
+      description: "keyboard replacement",
+      "tax-rate": 16,
+      price: 25.0,
     },
   ],
-  bottomNotice: "Thank You for your Business.",
+
+  // We will use bottomNotice to add a message of choice to the bottom of our invoice
+  bottomNotice: "Thank you for your Business!!",
+
+  // Here you can customize your invoice dimensions, currency, tax notation, and number formatting based on your locale
+  settings: {
+    currency: "USD", // See documentation 'Locales and Currency' for more info. Leave empty for no currency.
+
+    locale: "en-US", // Defaults to en-US, used for number formatting (See documentation 'Locales and Currency')
+    "tax-notation": "vat", // Defaults to 'vat'
+    // Using margin we can regulate how much white space we would like to have from the edges of our invoice
+    "margin-top": 25, // Defaults to '25'
+    "margin-right": 25, // Defaults to '25'
+    "margin-left": 25, // Defaults to '25'
+    "margin-bottom": 25, // Defaults to '25'
+    format: "A4", // Defaults to A4, options: A3, A4, A5, Legal, Letter, Tabloid
+    height: "1000px", // allowed units: mm, cm, in, px
+    width: "500px", // allowed units: mm, cm, in, px
+    // orientation: "landscape", // portrait or landscape, defaults to portrait
+  },
+
+  /*
+      Customize enables you to provide your own templates.
+      Please review the documentation for instructions and examples.
+      Leave this option blank to use the default template
+   */
+  customize: {
+    // template: fs.readFileSync("template.html", "base64"), // Must be base64 encoded html
+  },
 };
-
-//Executes the creation of the PDF
-easyinvoice.createInvoice(data, async function (result) {
-  //The response will contain a base64 encoded PDF file
-  console.log(result.pdf);
-
-  await fs.writeFileSync("invoice.pdf", result.pdf, "base64");
+easyinvoice.createInvoice(data, function (result) {
+  fs.writeFileSync("invoice.pdf", result.pdf, "base64");
 });
